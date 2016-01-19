d91320e49317e6e766e2d5b80f4a437ed10fd712
vickydasta <vickydasta@gmail.com>
2016-01-19 13:14:15 +0700

    add: bot.go

---
+++ bot.go
@@ -0,0 +1,49 @@
+// go telegram bot to check remote server 
+// to install telebot, run: go get github.com/tucnak/telebot
+
+package main 
+
+import (
+    "time"
+    "github.com/tucnak/telebot"
+    "os"
+    "os/exec"
+)
+
+func cmd(command string)string{
+    var (
+        cmdOut []byte
+        err     error
+    )
+    cmdOut, err = exec.Command(command).Output(); if err != nil{
+        os.Exit(1)
+    }
+    return string(cmdOut)
+}
+
+
+func main() {
+    var (
+        cmdOut []byte 
+        err     error
+    )
+
+    bot, err := telebot.NewBot(botToken)
+    if err != nil {
+        return
+    }
+
+    messages := make(chan telebot.Message)
+    bot.Listen(messages, 1*time.Second)
+
+    for message := range messages {
+        if message.Text == "/status" {
+            uptime := cmd("uptime")
+            bot.SendMessage(message.Chat, uptime, nil)
+        }
+        if message.Text == "/dir"{
+            ls := cmd("ls")
+            bot.SendMessage(message.Chat, ls, nil)
+        }
+    }
+}
