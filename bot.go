aaaf52e61ab0cccfcd0493eb8a112c15c8d44dfc
vickydasta <vickydasta@gmail.com>
2016-01-19 13:25:02 +0700

    better error handling at cmd()

--- bot.go
+++ bot.go
@@ -6,7 +6,7 @@ package main
 import (
     "time"
     "github.com/tucnak/telebot"
-    "os"
+    "log"
     "os/exec"
 )
 
@@ -16,23 +16,19 @@ func cmd(command string)string{
         err     error
     )
     cmdOut, err = exec.Command(command).Output(); if err != nil{
-        os.Exit(1)
+        log.Fatal(err)
     }
     return string(cmdOut)
 }
 
 
 func main() {
- 
-
     bot, err := telebot.NewBot(botToken)
     if err != nil {
         return
     }
-
     messages := make(chan telebot.Message)
     bot.Listen(messages, 1*time.Second)
-
     for message := range messages {
         if message.Text == "/status" {
             uptime := cmd("uptime")
