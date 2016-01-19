b9bc1a3073834fad025e294c728eb6ae83f643c8
vickydasta <vickydasta@gmail.com>
2016-01-19 16:16:53 +0700

    add: sizestring.cpp

---
+++ sizestring.cpp
@@ -0,0 +1,11 @@
+// string::size
+#include <iostream>
+#include <string>
+
+int main ()
+{
+  std::string str ("Hello, World!");
+  std::cout << "Hello, World!";
+  std::cout << "The size of str is " << str.size() << " bytes.\n";
+  return 0;
+}
