86985ed5755557e6cadfade3b64d729634325c0b
vickydasta <vickydasta@gmail.com>
2016-02-09 20:55:42 +0700

    new file:   autocomplete.py

---
+++ autocomplete.py
@@ -0,0 +1,25 @@
+# reference: http://blog.doughellmann.com/2008/11/pymotw-readline.html
+import readline
+
+class MyCompleter(object):  # Custom completer
+
+    def __init__(self, options):
+        self.options = sorted(options)
+
+    def complete(self, text, state):
+        if state == 0:  # on first trigger, build possible matches
+            if text:  # cache matches (entries that start with entered text)
+                self.matches = [s for s in self.options 
+                                    if s and s.startswith(text)]
+            else:  # no text entered, all matches possible
+                self.matches = self.options[:]
+
+        # return match indexed by state
+        try: 
+            return self.matches[state]
+        except IndexError:
+            return None
+
+completer = MyCompleter(['netstat','whoami','dir','ls','df','con','clear','serall'])
+readline.set_completer(completer.complete)
+readline.parse_and_bind('tab: complete')
