68e1e33ffd1a3888e47655165d94c64b7caccebd
Vicky Dasta <7861544+vck@users.noreply.github.com>
2019-01-05 10:41:09 +0700

    Create pythonenv.sh

---
+++ pythonenv.sh
@@ -0,0 +1,15 @@
+# install python 3
+sudo apt install python3
+
+# install pip3
+sudo apt install python3-pip
+
+# install virtualenv
+pip3 install virtualenv
+
+# create virtual env
+virtualenv env 
+
+# activate environment
+source env/bin/activate
+
